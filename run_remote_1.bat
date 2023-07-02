0e5e125c77d9a3041a9751910ce88be593696af2
Cell9870 <cellschool2020@gmail.com>
2023-07-02 00:50:51 -0300

    Prueba cluster 1 - escenario 2

--- run_remote_1.bat
+++ run_remote_1.bat
@@ -1,4 +1,4 @@
-for /l %%x in (1, 1, 20) do (.\automaton.exe > .\out\remote\secuencial\scenario1\%%x.txt) 
-for /l %%x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi.exe > .\out\remote\mpi\scenario1\%%x.txt)
-for /l %%x in (1, 1, 20) do (.\automaton_omp.exe > .\out\remote\openmp\scenario1\%%x.txt)
-for /l %%x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi_omp.exe > .\out\remote\mixto\scenario1\%%x.txt)
+for /l %x in (1, 1, 20) do (.\automaton.exe > .\out\remote\secuencial\scenario1\%x.txt) 
+for /l %x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi.exe > .\out\remote\mpi\scenario1\%x.txt)
+for /l %x in (1, 1, 20) do (.\automaton_omp.exe > .\out\remote\openmp\scenario1\%x.txt)
+for /l %x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi_omp.exe > .\out\remote\mixto\scenario1\%x.txt)
--- run_remote_2.bat
+++ run_remote_2.bat
@@ -1,4 +1,4 @@
-for /l %%x in (1, 1, 20) do (.\automaton.exe > .\out\remote\secuencial\scenario2\%%x.txt) 
-for /l %%x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi.exe > .\out\remote\mpi\scenario2\%%x.txt)
-for /l %%x in (1, 1, 20) do (.\automaton_omp.exe > .\out\remote\openmp\scenario2\%%x.txt)
-for /l %%x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi_omp.exe > .\out\remote\mixto\scenario2\%%x.txt)
+for /l %x in (1, 1, 20) do (.\automaton.exe > .\out\remote\secuencial\scenario2\%x.txt) 
+for /l %x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi.exe > .\out\remote\mpi\scenario2\%x.txt)
+for /l %x in (1, 1, 20) do (.\automaton_omp.exe > .\out\remote\openmp\scenario2\%x.txt)
+for /l %x in (1, 1, 20) do (mpiexec -np 4 .\automaton_mpi_omp.exe > .\out\remote\mixto\scenario2\%x.txt)
