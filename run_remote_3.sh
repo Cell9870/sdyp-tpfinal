7184c8b6ba9ae28fe8f374804b8ad8d1eaf4a3b7
Cell9870 <cellschool2020@gmail.com>
2023-07-02 01:28:36 -0300

    escenario 2 - FINAL

--- run_remote_3.sh
+++ run_remote_3.sh
@@ -2,20 +2,24 @@
 
 for ((i=1;i<=20;i++))
 do
+    echo "iteration $i: secuencial"
     ./automaton.exe > ./out/remote/secuencial/scenario3/$i.txt
 done
 
 for ((i=1;i<=20;i++))
 do
+    echo "iteration $i: mpi"
     mpirun -np 4 ./automaton_mpi.exe -machinefile hostfile.txt > ./out/remote/mpi/scenario3/$i.txt
 done
 
 for ((i=1;i<=20;i++))
 do
+    echo "iteration $i: openmp"
     ./automaton_omp.exe > ./out/remote/openmp/scenario3/$i.txt
 done
 
 for ((i=1;i<=20;i++))
 do
+    echo "iteration $i: mixto"
     mpirun -np 4 ./automaton_mpi_omp.exe -machinefile hostfile.txt > ./out/remote/mixto/scenario3/$i.txt
 done
